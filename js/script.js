63c5cd98b258f93ea31e0574d4eccdf89d1d5bcf
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-22 17:51:53 +0100

    created one unified function

--- js/script.js
+++ js/script.js
@@ -12,21 +12,26 @@ const app = createApp({
         lastElementIndex() {
             return this.destinations.length - 1;
         },
+
         isFirstIndex(){
             return this.currentIndex === 0;
         },
+
         isLastIndex(){
             return this.currentIndex === this.lastElementIndex;
         }
     },
     methods: {
-        goToPrev() {
-            if (this.isFirstIndex) this.currentIndex = this.lastElementIndex;
-            else this.currentIndex--;
-        },
-        goToNext() {
-            if (this.isLastIndex) this.currentIndex = 0;
-            else this.currentIndex++;
+        setCurrentIndex(target) {
+            if (target === 'next') {
+                if (this.isLastIndex) this.currentIndex = 0;
+                else this.currentIndex++;    
+            } else if (target === 'prev') {
+                if (this.isFirstIndex) this.currentIndex = this.lastElementIndex;
+                else this.currentIndex--;
+            } else {
+                this.currentIndex = target;
+            }
         }
     }
 });
