a32e1dabdf75e2faac3994728986ff57079a7c56
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-22 17:41:35 +0100

    added on click into thumbnails images

--- js/script.js
+++ js/script.js
@@ -5,8 +5,30 @@ const {createApp} = Vue;
 const app = createApp({
     name: 'VueCarousel',
     data: () => ({
-        destinations
-    })
+        destinations,
+        currentIndex: 0
+    }),
+    computed: {
+        lastElementIndex() {
+            return this.destinations.length - 1;
+        },
+        isFirstIndex(){
+            return this.currentIndex === 0;
+        },
+        isLastIndex(){
+            return this.currentIndex === this.lastElementIndex;
+        }
+    },
+    methods: {
+        goToPrev() {
+            if (this.isFirstIndex) this.currentIndex = this.lastElementIndex;
+            else this.currentIndex--;
+        },
+        goToNext() {
+            if (this.isLastIndex) this.currentIndex = 0;
+            else this.currentIndex++;
+        }
+    }
 });
 
 app.mount('#root');
